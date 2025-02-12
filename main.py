6f5d799af930f08d6f4bde7ad074f33a8a97a478
Dima7100 <dmpr347@gmail.com>
2025-02-12 23:21:07 +0300

    Частично выполнил TODO

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 from okmcko import get_response
 from files_processing import get_data
+from file_storage import filter_new_files
+from downloader import download_files
 
 if __name__ == '__main__':
-    response = get_response()
-    data = get_data(response)
 
     #TODO теперь, когда мы все получили. Нужна функция со сверкой имеющейся data. Если data.json нет, то создать.
     #TODO а main должен скачать файлы, вернуть список того, что можно отдавать боту и вместо href - пути к файлам и дополнить data.json
     # ещё можно из okmcko убрать все, что связано с токеном мос.ру. В мос.ру из окмцко засунуть проверку токена, загрузку токена и сохранение токена.
-    '''
-    И будет типа 
-    token = load_token()
-    if is_token_expire():
-        token = get_token()
-        save_token(token)
-    response = get_response()
+
+
+    #token = load_token()
+    #if is_token_expire():
+        #token = get_token()
+        #save_token(token)
+    response, session = get_response()
     data = get_data(response)
-    new_data = exclude_old(data)
-    download_files(new_data)
+    new_data = filter_new_files(data)
+    download_files(session, new_data)
     
     
     
-    '''
