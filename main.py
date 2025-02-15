de73e6200bdd5fb17d33e36f1c26019659bb735e
Dima7100 <dmpr347@gmail.com>
2025-02-15 03:29:42 +0300

    Добавил условие на будущее

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from okmcko import get_response
-from files_processing import get_data
-from file_storage import filter_new_files
+from data_parsing import get_data
+from data_filter import filter_new_files
 from downloader import download_files
 
 if __name__ == '__main__':
 
     #TODO теперь, когда мы все получили. Нужна функция со сверкой имеющейся data. Если data.json нет, то создать.
     #TODO а main должен скачать файлы, вернуть список того, что можно отдавать боту и вместо href - пути к файлам и дополнить data.json
-    # ещё можно из okmcko убрать все, что связано с токеном мос.ру. В мос.ру из окмцко засунуть проверку токена, загрузку токена и сохранение токена.
+    #TODO ещё можно из okmcko убрать все, что связано с токеном мос.ру. В мос.ру из окмцко засунуть проверку токена, загрузку токена и сохранение токена.
+
+    #TODO new нет смысла менять href на путь файла, когда путь и так известен.
 
 
     #token = load_token()
@@ -17,7 +19,10 @@ if __name__ == '__main__':
     response, session = get_response()
     data = get_data(response)
     new_data = filter_new_files(data)
-    download_files(session, new_data)
+    if new_data:
+        download_files(session, new_data)
+    else:
+        return None
     
     
     
