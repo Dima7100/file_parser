54cbaf6ddb3d8e7a4e5b95a5878589e3660d46d2
Dima7100 <dmpr347@gmail.com>
2025-03-03 23:46:43 +0300

    Добавил новые функции

--- configs/__init__.py
+++ configs/__init__.py
@@ -1,2 +1,2 @@
 from configs import config
-from .logging_config import logger_mcko, logger_storage, logger_processing, logger_mos, logger_downloader
+from .logging_config import logger_mcko, logger_storage, logger_processing, logger_mos, logger_downloader, logger_bot
--- parser/__init__.py
+++ parser/__init__.py
@@ -2,4 +2,5 @@ from .data_filter import filter_new_files
 from .data_parsing import get_data
 from .downloader import download_files
 from .get_mos_token import get_token
-from .okmcko import is_token_expired, start_session, update_headers, get_mcko_token, get_mcko_auth, get_response
+from .okmcko import is_token_expired, start_session, update_headers, get_mcko_token, get_mcko_auth, get_response, session_status
+from .coordinator import get_mcko_session, get_data_to_bot
