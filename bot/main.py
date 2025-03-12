d0d216f4bbe42aa3691d04a572fb5a7d865b040a
Dima7100 <dmpr347@gmail.com>
2025-03-13 01:02:54 +0300

    Добавил пару функций в старт бота

--- bot/main.py
+++ bot/main.py
@@ -4,9 +4,10 @@ import os
 from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
 from bot.handlers import router as handlers_router
-from database import init_db, add_user
+from database import init_db, add_user, add_subscribe
 from bot.services import check_and_send_data
 from configs.config import ADMIN_ID
+from bot.services import set_commands
 
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
@@ -16,10 +17,15 @@ bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 dp.include_router(handlers_router)
 
+
 async def main():
     await init_db()
-    await add_user(ADMIN_ID, 'Админ', 'Админов', 'Admin')
+    await set_commands(bot)
+    await add_user(ADMIN_ID, 'Админ', 'Админов', 'Admin', 'approved')
+    await add_subscribe('МЦКО')
+
     await asyncio.gather(dp.start_polling(bot), check_and_send_data(bot))
+
 
 
 if __name__ == '__main__':
